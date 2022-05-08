2d4132663afc098d20e0a96813cc40e9c6a67e2e
CRANKV2 <97807862+CRANKV2@users.noreply.github.com>
2022-05-08 04:35:00 +0200

    Add files via upload
    v2.2

--- customize.sh
+++ customize.sh
@@ -7,7 +7,23 @@ ui_print "⚡️Simple Res. Changer ⚡️"
 ui_print "***************************"
 sleep 2
 ui_print "⚡️by CV2 (CRANKV2 @ GitHub)⚡️"
+sleep 2
+ui_print "⚡️Extracting all Files⚡️"
 ui_print ""
+sleep 3
+ui_print "⚡️DONE!⚡️"
+ui_print ""
+SLEEP 2
+ui_print "⚡️Use ' su -c SRC ' In TERMUX⚡️"
+sleep 1
+ui_print ""
+ui_print "⚡️To entering Menu!⚡️"
+ui_print "⚡️Join @Android Root Modules Community⚡️"
+ui_print "⚡️on Telegram for more good Stuff!⚡️"
+ui_print ""
+ui_print ""
+ui_print ""
+
 
 set_permissions() {
   set_perm_recursive $MODPATH/system/bin root root 0777 0755
