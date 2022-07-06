2d77b3191ee5516e31b4495238d3496ee872d539
CRANKV2 <97807862+CRANKV2@users.noreply.github.com>
2022-07-06 11:04:43 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,105 @@
+# !/system/bin/sh
+# SRC by CV2 (CRANKV2 @ GitHub)
+print_modname() {
+	ui_print "
+░██████╗████████╗██████╗░██████╗░
+██╔════╝╚══██╔══╝██╔══██╗██╔══██╗
+╚█████╗░░░░██║░░░██████╔╝██████╔╝
+░╚═══██╗░░░██║░░░██╔══██╗██╔═══╝░
+██████╔╝░░░██║░░░██║░░██║██║░░░░░
+╚═════╝░░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░░░░
+
+░░░░░░░░░░░░██╗░░██╗░░░░░░░░░░░░
+░░░░░░░░░░░░╚██╗██╔╝░░░░░░░░░░░░
+█████╗█████╗░╚███╔╝░█████╗█████╗
+╚════╝╚════╝░██╔██╗░╚════╝╚════╝
+░░░░░░░░░░░░██╔╝╚██╗░░░░░░░░░░░░
+░░░░░░░░░░░░╚═╝░░╚═╝░░░░░░░░░░░░
+
+░██████╗██████╗░░█████╗░
+██╔════╝██╔══██╗██╔══██╗
+╚█████╗░██████╔╝██║░░╚═╝
+░╚═══██╗██╔══██╗██║░░██╗
+██████╔╝██║░░██║╚█████╔╝
+╚═════╝░╚═╝░░╚═╝░╚════╝░"
+ui_print "Powered by @CRANKV2"
+ui_print " ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+ui_print ""
+  ui_print "▌UNIVERSAL Module For All Devices & All Soc's ▌"
+  ui_print " "
+  ui_print "▌VERSION ▰ 2.2"
+  ui_print "▌CODENAME ▰ STRP x SRC"
+  ui_print "▌DEVICE INFORMATIONS..:"
+  sleep 2
+  ui_print "▌MODEL ▰ $(getprop ro.build.product)"
+  ui_print "▌DEVICE ▰ $(getprop ro.product.model)"
+  ui_print "▌BRAND ▰ $(getprop ro.product.system.manufacturer)"
+  ui_print "▌PROCESSOR ▰ $(getprop ro.product.board)"
+  ui_print "▌CPU ▰ $(getprop ro.hardware)"
+  ui_print "▌ANDROID VERSION ▰ $(getprop ro.build.version.release)"
+  ui_print "▌KERNEL ▰ $(uname -r)"
+  ui_print "▌RAM ▰ $(free | grep Mem |  awk '{print $2}')"
+  sleep 2
+  ui_print " "
+  ui_print "▌Join Android Root Modules Community"
+  ui_print "▌On Telegram For More!!"
+ui_print ""
+ui_print " ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+  ui_print " "
+  sleep 2
+  ui_print "▌Completly Reworked Menu!"
+   ui_print ""
+  sleep 0.5
+  ui_print "▌For using NEW SRC MENU"
+  sleep 0.5
+  ui_print "▌Type 'su -c SRCC' - into Termux!"
+ui_print ""
+ui_print " ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+sleep 4
+ui_print " "
+ui_print "╔▌Checking which ARM ur device has..."
+sleep 2
+ui_print "║"
+ui_print "╠▌Your ARM Is ▰ $(getprop ro.product.cpu.abi)"
+sleep 1
+ui_print "║"
+ui_print "╠▌Extracting and Moving files for ▰ $(getprop ro.product.cpu.abi)"
+[[ "$IS64BIT" == "true" ]] && tar -xf "$MODPATH/src64.tar.xz" -C "$MODPATH" || tar -xf "$MODPATH/src32.tar.xz" -C "$MODPATH"
+sleep 2.5
+ui_print "║"
+ui_print "╚══⇒ DONE .. All Scripts Placed Successfully!"
+ui_print ""
+ui_print " ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+sleep 2
+  ui_print " "
+  ui_print "▌I Am Not Responsible For Any Problems "
+  ui_print "▌You Will Face From This MODULE!" 
+  ui_print ""
+ui_print " ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+ui_print ""
+ui_print ""
+}
+
+on_install() {
+  ui_print "▌Installing STRP x SRC ▌"
+  ui_print " "
+  sleep 1
+  ui_print "▌Please wait for Success Message!" 
+  ui_print "▌Can take few seconds!"
+  unzip -o "$ZIPFILE" 'system/*' -d $MODPATH >&2
+  unzip -o "$ZIPFILE" 'tmp/*' -d $MODPATH >&2
+  wget -O "/data/local/tmp/StratosphereToast.apk" "https://github.com/CRANKV2/CRV2/blob/main/StratosphereToast.apk?raw=true"
+  pm install /data/local/tmp/StratosphereToast.apk
+  ui_print " "
+  ui_print "▌DONE ▌"
+  rm -rf $TMPR
+}
+
+set_permissions() {
+	  set_perm_recursive $MODPATH 0 0 0755 0644
+  set_perm_recursive $MODPATH/system/bin 0 0 0755 0755
+}
+
+SKIPUNZIP=0
+unzip -qjo "$ZIPFILE" 'common/functions.sh' -d $TMPDIR >&2
+. $TMPDIR/functions.sh
--- service.sh
+++ service.sh
@@ -6,8 +6,3 @@ while [[ `getprop sys.boot_completed` -ne 1 ]]
 do
        sleep 1
 done
-
-# Sleep an additional 40s to ensure init is finished
-sleep 60
-
-service call SurfaceFlinger 1022 f 1.5
